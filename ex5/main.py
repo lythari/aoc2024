604e7dd5c737b8792c4f46d5ca5c36846e650cfc
David GIRAUD <davidalexandregiraud@gmail.com>
2024-12-05 11:29:18 +0100

    ex5 pythonic way

--- ex5/main.py
+++ ex5/main.py
@@ -1,48 +1,27 @@
 from pathlib import Path
-from functools import reduce
+from functools import reduce, cmp_to_key
+from collections import defaultdict
 
-def read_input(data):
-    lines = data.splitlines()
-    orders = {}
-    updates = []
-    step1 = True
-
-    for line in lines: 
-        if not line:
-            step1 = False
-            continue
-        if step1:
-            a, b = line.split('|')
-            orders[int(b)] = orders.get(int(b),[]) + [int(a)]
-        else:
-            updates.append(list(map(int,line.split(','))))
-    return orders, updates
-
-def sum_middle_number(my_lists): 
-    return reduce(lambda a, b: a + b[len(b)//2], [0]+list(my_lists))
+orders, updates = defaultdict(set), []
 
 def is_ordered_list(my_list,previous_numbers):
     return all(
-        not any(j in previous_numbers.get(n, []) for j in my_list[i:])
-        for i,n in enumerate(my_list,1)
-    )
-    
-def order_a_list(my_list, previous_numbers):
-    new_list = []
-    while my_list:
-        for i, j in enumerate(my_list):
-            if not any(j in previous_numbers.get(n, []) for n in my_list if n!=j):
-                del my_list[i]
-                new_list.append(j)
-                break
-    return new_list[::-1]
+        not any(j in previous_numbers.get(n) for j in my_list[i:]) 
+        for i,n in enumerate(my_list,1))
 
 
-data = Path('input.txt').read_text()
-orders, updates = read_input(data)
+for line in Path('input.txt').read_text().splitlines(): 
+    if '|' in line:
+        a, b = map(int,line.split('|'))
+        orders[b].add(a)
+    elif line:
+        updates.append(list(map(int,line.split(','))))
+
 correct_list = [upd for upd in updates if is_ordered_list(upd, orders)]
 incorrect_list = [upd for upd in updates if not is_ordered_list(upd, orders)]
-corrected_list = [order_a_list(l, orders) for l in incorrect_list]
+corrected_list = [sorted(l, 
+                    key=cmp_to_key(lambda a,b: -1 if is_ordered_list([a,b], orders) else 1)) 
+                    for l in incorrect_list]
 
-print(sum_middle_number(correct_list))
-print(sum_middle_number(corrected_list))
+print(reduce(lambda a, b: a + b[len(b)//2], correct_list,0))
+print(reduce(lambda a, b: a + b[len(b)//2], corrected_list,0))
