711a06adbd1c13725cd6d810837eea523dda197e
David GIRAUD <davidalexandregiraud@gmail.com>
2024-12-07 06:43:21 +0100

    Exo 7

--- ex7/main.py
+++ ex7/main.py
@@ -14,6 +14,4 @@ def is_possible(r, l, s= None):
     return is_possible(r, l.copy(), (s[0]+cur, s[0]*cur, int(str(s[0])+str(cur)))) or is_possible(r, l.copy(), (s[1]+cur, s[1]*cur, int(str(s[1])+str(cur)))) or is_possible(r, l.copy(), (s[2]+cur, s[2]*cur, int(str(s[2])+str(cur))))
 
 data = Path('input.txt').read_text().splitlines()
-print(sum(int(a) 
-        for a,b in (line.split(':') for line in data )
-        if is_possible(int(a), list(map(int,b.split())))))
+print(sum(int(a) for a,b in (line.split(':') for line in data ) if is_possible(int(a), list(map(int,b.split())))))
