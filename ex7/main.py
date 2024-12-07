968d3215dee8b330c7ac77e4d0d891a8a3c1e14b
David GIRAUD <davidalexandregiraud@gmail.com>
2024-12-07 08:59:53 +0100

    remove oneliners

--- ex7/main.py
+++ ex7/main.py
@@ -3,16 +3,20 @@ from operator import mul, add
 
 concat = lambda a,b: int(str(a)+str(b))
 
-def is_possible(r, l, ops, s= None):
+def is_possible(r, l, ops, s):
     if not l:
         return r in s
-    cur = l[0]
-    if s is None:
-        return is_possible(r, l[1:], ops, [cur for _ in ops])
-    if len(l) == 1:
-        return any(op(i,cur) == r for i in s for op in ops)
-    return any(is_possible(r, l[1:], ops, [op(i, cur) for op in ops]) for i in s)
+    return any(is_possible(r, l[1:], ops, [op(i, l[0]) for op in ops]) for i in s)
+
+def sum_list(data, ops):
+    for line in data:
+        a,b = line.split(':')
+        a = int(a)
+        b = list(map(int,b.split()))
+        if is_possible(a, b[1:], ops, [b[0] for _ in ops]):
+            yield a
 
 data = Path('input.txt').read_text().splitlines()
-print(sum(int(a) for a,b in (line.split(':') for line in data ) if is_possible(int(a), list(map(int,b.split())), [add, mul])))
-print(sum(int(a) for a,b in (line.split(':') for line in data ) if is_possible(int(a), list(map(int,b.split())), [add, mul, concat])))
+
+print(sum(sum_list(data, [add, mul])))
+print(sum(sum_list(data, [add, mul, concat])))
