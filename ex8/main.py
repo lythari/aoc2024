4b51c908d31632684e00b9742a8789cb27d9fab1
David GIRAUD <davidalexandregiraud@gmail.com>
2024-12-08 09:49:38 +0100

    cleaner and pythoner day8

--- ex8/main.py
+++ ex8/main.py
@@ -1,87 +1,40 @@
 from pathlib import Path
 from collections import defaultdict
 from itertools import combinations
-from math import ceil, floor
 
-data = Path('input.txt').read_text().splitlines()
 
-antenna_map = defaultdict(set)
-frequencies = set()
+def diagonals(positions):
+    x1, y1, x2, y2 = positions
+    dx, dy = (x1-x2)/(y1-y2),(y1-y2)/(x1-x2)
+    directions = [(1,dy),(-1,-dy),(dx,1),(-dx,-1)]
+    for da, db in directions:
+        test = x1, y1
+        while True:
+            x, y = test[0]+da, test[1]+db
+            test = x,y
+            if not 0<=round(x)<=size[0] or not 0<=round(y)<=size[1]:
+                break
+            if abs(round(x)-x) > 1/10000 or abs(round(y)-y) > 1/10000:
+                continue
+            yield int(round(x)),int(round(y))
 
-for y,line in enumerate(data):
+
+antenna_map, frequencies, antinodes, resonance = defaultdict(set), set(), set(), set()
+
+for y,line in enumerate(Path('input.txt').read_text().splitlines()):
     for x,char in enumerate(line):
         antenna_map[(x,y)]=char
         if char != '.':
             frequencies.add(char)
-        
 size = x,y
-antinodes = set()
-
-def diagonals(pos1, pos2):
-    x1, y1 = pos1
-    x2, y2 = pos2
-    dy = (y1-y2)/(x1-x2)
-    test = x1, y1
-    while True:
-        x = test[0]+1
-        y = test[1]+dy
-        if not 0<=x<=size[0] or not 0<=round(y)<=size[1]:
-            break
-        if abs(round(y)-y) > 1/10000:
-            test = x,y
-            continue
-        yield x,int(round(y))
-        test = x,y
-        
-   
-    test = x1, y1
-    while True:
-        x = test[0]-1
-        y = test[1]-dy
-        if not 0<=x<=size[0] or not 0<=round(y)<=size[1]:
-            break
-        if abs(round(y)-y) > 1/10000:
-            test = x,y
-            continue
-        yield x,int(round(y))
-        test = x,y
-            
-    dx = (x1-x2)/(y1-y2)
-    test = x1, y1
-    while True:
-        x = test[0]+dx
-        y = test[1]+1
-        print(x,y)
-        if not 0<=round(x)<=size[0] or not 0<=y<=size[1]:
-            break
-        if abs(round(x)-x) > 1/10000:
-            test = x,y
-            continue
-        yield int(round(x)),y
-        test = x,y
-   
-    test = x1, y1
-    while True:
-        x = test[0]-dx
-        y = test[1]-1
-        print(x,y)
-        if not 0<=round(x)<=size[0] or not 0<=y<=size[1]:
-            break
-        if abs(round(x)-x) > 1/10000:
-            test = x,y
-            continue
-        yield int(round(x)),y
-        test = x,y
 
 for f in frequencies:
-    print(f)
     for a, b in combinations([k for k, i in antenna_map.items() if i == f], 2):
-        antinodes.add(a)
-        antinodes.add(b)
+        resonance.add(a)
+        resonance.add(b)
         x1, y1, x2, y2 = *a, *b
-        
-        for node in diagonals((x1,y1),(x2,y2)):
-            antinodes.add(node)
-
-print(sorted(antinodes))
+        dx, dy = x1-x2, y1-y2
+        antinodes.update((n for n in [(x1+dx,y1+dy), (x2-dx,y2-dy)] if n in antenna_map))
+        resonance.update(diagonals((x1,y1,x2,y2)))
 print(len(antinodes))
+print(len(resonance))
